6ae787d341d036eb9a1cea49b0956006db0b5fa7
emon-hossain7 <emon.hossain.wd@gmail.com>
2022-08-22 12:49:04 +0600

    complate html css work

---
+++ js/main.js
@@ -0,0 +1,51 @@
+const btnCalculate = document.getElementById('btn-calculate');
+btnCalculate.addEventListener('click', () => {
+    const perPlayerInput = document.getElementById('per-player');
+    const perPlayer = parseFloat(perPlayerInput.value);
+
+    
+    console.log(perPlayer)
+
+    const playerExpensesInput = document.getElementById('player-expenses');
+    const playerExpenses = p;
+    
+    
+
+
+
+
+
+
+
+
+
+
+    console.log('hello btn')
+    perPlayerInput.value= '';
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
