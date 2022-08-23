325109e085d6d0619fcecc6ae77756fc1e19bf43
emon-hossain7 <emon.hossain.wd@gmail.com>
2022-08-23 12:17:24 +0600

    validation is complate

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
 const playersItems = [];
-function display(selectPlayers) {
+function display(selectPlayers, element) {
     if (selectPlayers.length <= 5) {
         document.getElementById('total-selected').innerText = selectPlayers.length
         const playerList = document.getElementById('player-list');
         playerList.innerText = '';
+        let count = 1;
         for (player of selectPlayers) {
+
             const playerName = player.playerName;
             const li = document.createElement('li');
-            
-            li.innerText = playerName;
+
+            li.innerHTML = `
+            ${count}. ${playerName}
+            `;
             playerList.appendChild(li)
+            count++;
         }
-    
+        element.setAttribute('disabled', true)
     }
     else {
-        alert('You have five player selected')
-        return;
+        alert('enough selected')
     }
-
 }
 
 function selectedItem(element) {
@@ -26,33 +29,47 @@ function selectedItem(element) {
         playerName: playerName,
     };
     playersItems.push(playerObj)
-    display(playersItems)
+    display(playersItems, element)
 }
-
-
-
 
 const btnCalculate = document.getElementById('btn-calculate');
 btnCalculate.addEventListener('click', () => {
+    const totalSelectedPlayer = document.getElementById('total-selected').innerText;
+    const totalSelectedPlayerValue = parseInt(totalSelectedPlayer);
+    console.log(typeof totalSelectedPlayerValue)
     const perPlayerInput = document.getElementById('per-player');
     const perPlayer = parseInt(perPlayerInput.value);
-
     const playerExpensesInput = document.getElementById('player-expenses');
-    const playerExpenses = playerExpensesInput.innerText;
-    console.log(playerExpenses, perPlayer)
-    playerExpensesInput.innerText = perPlayer;
-
-    perPlayerInput.value = '';
-    playerExpensesInput.value = '';
-
-
+    const perPlayerCost = totalSelectedPlayerValue * perPlayer;
+    if(isNaN(perPlayerCost)){
+        alert('is not a number')
+        return;
+    }
+    playerExpensesInput.innerText = perPlayerCost;
 })
 
 
+const totalCostBtn = document.getElementById('total-cost-btn');
+totalCostBtn.addEventListener('click', function () {
+    console.log('btn click')
+    const managerField = document.getElementById('manager-field');
+    const managerCost = parseInt(managerField.value);
+    const coachField = document.getElementById('coach-field');
+    const coachCost = parseInt(coachField.value);
+    const playerExpensesField = document.getElementById('player-expenses');
+    const playerExpenses = parseInt(playerExpensesField.innerText);
+    const totalCostCalculate = managerCost + coachCost + playerExpenses;
+    if(isNaN(totalCostCalculate)){
+        alert('is not a number')
+        return;
+    }
+    const totalCost = document.getElementById('total-cost');
+    totalCost.innerText = totalCostCalculate
 
-
-// 1: 40 class
-
+    managerField.value = '';
+    coachField.value = '';
+    console.log(totalCost)
+})
 
 
 
